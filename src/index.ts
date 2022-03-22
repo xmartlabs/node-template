7be79398de31801524154264bc22dd6469c891bd
Santiago Chabert <santiago.chabert@xmartlabs.com>
2022-03-22 17:22:33 -0300

    Moved routes to routes folder

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
-import { usersRouter } from './routes';
+import routes from './routes';
 
 const app = express();
 const port = 8080;
 
-app.use('/users', usersRouter);
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,8 @@
-export { usersRouter } from './users';
+import { Router } from 'express';
+import { usersRouter } from './users';
+
+const routes = Router();
+
+routes.use('/users', usersRouter);
+
+export default routes;
