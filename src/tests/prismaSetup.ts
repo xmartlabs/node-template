399f525fe44fc06a0a3b1b767a0d474eae963406
Santiago Chabert <santiago.chabert@xmartlabs.com>
2022-03-30 10:27:14 -0300

    Change prisma file to tests folder

--- src/tests/prismaSetup.ts
+++ src/tests/prismaSetup.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { mockDeep, mockReset } from 'jest-mock-extended';
 import { DeepMockProxy } from 'jest-mock-extended/lib/cjs/Mock';
 
-import prisma from './client';
+import prisma from '../../prisma/client';
 
 jest.mock('./client', () => ({
   __esModule: true,
