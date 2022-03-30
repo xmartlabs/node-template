db5c96dcd76370a34a680886b8812f0a8df83c40
Santiago Chabert <santiago.chabert@xmartlabs.com>
2022-03-30 11:48:44 -0300

    Change jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.ts?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   resolver: 'jest-node-exports-resolver',
   testTimeout: 20000,
 };
