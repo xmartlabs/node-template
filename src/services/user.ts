6036b0494717065516a96333150b59cce99861dc
Santiago Chabert <santiago.chabert@xmartlabs.com>
2022-04-07 12:36:56 -0300

    Added dockerfile and changes to make it work properly

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Prisma, User } from '@prisma/client';
 import prisma from '../../prisma/client';
 import { ApiError } from '../utils/apiError';
