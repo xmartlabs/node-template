502f94ef6a9ccc702189afe1a3a2a5a1392042e2
lucasparese <parese1995@gmail.com>
2023-08-08 22:06:33 -0300

    add cron mock ut

--- src/services/user.test.ts
+++ src/services/user.test.ts
@@ -2,16 +2,26 @@ import { prismaMock } from 'tests/prismaSetup';
 import { generateUserData } from 'tests/utils/generateData';
 import { UserService } from 'services/user';
 import { errors } from 'config/errors';
+import cron from 'node-cron';
 import { sendSignUpEmail } from 'emails/index';
+
+jest.mock('node-cron');
 
 jest.mock('emails/index');
 
 const mockSendSignUpEmail = sendSignUpEmail as jest.Mock;
 
 describe('User service: ', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   test('should create a new user with email', async () => {
+    const mockSchedule = jest.fn().mockImplementation((pattern, callback) => {
+      callback();
+    });
+    cron.schedule = mockSchedule;
     const userData = generateUserData();
-    // @ts-ignore
+
     prismaMock.user.create.mockResolvedValue(userData);
     prismaMock.user.update.mockResolvedValue(userData);
     mockSendSignUpEmail.mockResolvedValue(undefined);
