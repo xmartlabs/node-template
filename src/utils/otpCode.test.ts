21e5da5c8f8145cba9b18c070dadf0dd8ce5ac37
lucasparese <parese1995@gmail.com>
2023-11-15 12:02:57 -0300

    add generate code ut

--- src/utils/otpCode.test.ts
+++ src/utils/otpCode.test.ts
@@ -1,10 +1,14 @@
 import { errors } from 'config/errors';
 import { prismaMock } from 'tests/prismaSetup';
 import { ApiError } from 'utils/apiError';
-import { verifyCode } from 'utils/otpCode';
+import { verifyCode, generateOTPCode } from 'utils/otpCode';
+import * as securityService from 'services/security';
+
 import { generateUserData, generateOTPCodeData, generateOTPExpiredCodeData } from 'tests/utils/generateData';
 
 jest.mock('bcryptjs');
+
+jest.spyOn(securityService, 'generateCode').mockImplementation(() => '123456');
 
 const OTPExpiredCodeData = generateOTPExpiredCodeData();
 const OTPCodeData = generateOTPCodeData();
@@ -48,6 +52,28 @@ describe('verifyCode', () => {
         '123456',
         userData.id,
       )).resolves.toEqual(true);
+    });
+  });
+});
+
+describe('generateCode', () => {
+  describe('error cases', () => {
+    it('Unvalidated code already exists', () => {
+      prismaMock.oTP.findFirst.mockResolvedValue(OTPCodeData);
+      expect(
+        generateOTPCode(
+          userData.id,
+        ),
+      ).rejects.toThrowError(new ApiError(errors.UNVALIDATED_CODE_ALREADY_EXISTS));
+    });
+  });
+  describe('generate code successfully', () => {
+    it('generate code successfully', async () => {
+      prismaMock.oTP.findFirst.mockResolvedValue(null);
+      prismaMock.oTP.upsert.mockResolvedValue({ ...OTPCodeData, code: '123456' });
+
+      const code = await generateOTPCode(userData.id);
+      expect(code).toEqual('123456');
     });
   });
 });
