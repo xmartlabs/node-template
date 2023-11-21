9882b5785369a8a8beda13c9fc48b94a94cc4e44
lucasparese <parese1995@gmail.com>
2023-11-21 20:41:46 -0300

    export controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,5 @@
-export { UsersController } from 'controllers/users';
+//check this
+export { UsersControllerV1 } from 'controllers/users';
+export { AuthControllerV1 } from 'controllers/auth';
+export { HealthCheckControllerV1 } from 'controllers/healthcheck';
+
