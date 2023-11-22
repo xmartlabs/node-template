b12e5b3a97543575e22ef85ba15701835dd2c57a
lucasparese <parese1995@gmail.com>
2023-11-21 21:25:57 -0300

    delete unnecessary index file for export controllers

--- src/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-//check this
-export { UsersControllerV1 } from 'controllers/users';
-export { AuthControllerV1 } from 'controllers/auth';
-export { HealthCheckControllerV1 } from 'controllers/healthcheck';
-
